ccbe10d779ed5b3add633c835fc17ee904ba9c02
Trofmann <trofimovevgeniy9@gmail.com>
2021-10-17 21:24:24 +0300

    Новые константы

--- const.py
+++ const.py
@@ -1,3 +1,4 @@
+# Данные базы данных
 DB_NAME = 'd4j4mnjl0qr8lh'
 USER_NAME = 'postgres'
 PASSWORD = 'postgres'
@@ -5,9 +6,24 @@ HOST = '127.0.0.1'
 PORT = '5432'
 TABLE_NAME = 'TCUserData'
 
+# Поля, из которых извлекаем данные
 TABLE_FIELDS = [
     'id',
     'university_id',
     'university_data',
     'reg_time'
 ]
+
+INTEGER = 'integer'
+CHAR = 'field'
+JSON = 'json'
+DATETIME = 'timestamp'
+
+# Типы извлекаемых полей
+FIELDS_TYPES = {
+    'id': INTEGER,
+    'university_id': CHAR,
+    'university_data': JSON,
+    'reg_time': DATETIME,
+}
+
