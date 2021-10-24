8f20ab9bf1acb66471dbc4cb6101817a0ac55cc3
Trofmann <trofimovevgeniy9@gmail.com>
2021-10-24 20:05:16 +0300

    Полностью готова функция обрезки даты

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from const import DAY, YEAR, MONTH, HOUR, MINUTE, SECOND
+
+
 def tuple_to_dict(data_tuple: tuple, keys: list):
     """
     Преобразование кортежа в словарь
@@ -13,11 +18,26 @@ def tuple_to_dict(data_tuple: tuple, keys: list):
             data_dict.update({keys[ind]: data})
         return data_dict
 
-def trim_datetime(_datetime, trim):
+
+def trim_datetime(_datetime, trim=DAY):
     """
     Очистка даты до определенного момента
     :param _datetime: datetime.datetime
     :param trim: str
     :return: datetime
     """
-    pass
+    clean_datetime = None
+    if _datetime:
+        clean_datetime = datetime(_datetime.year, _datetime.month, _datetime.day)
+        if trim == YEAR:
+            clean_datetime = clean_datetime.replace(month=1, day=1)
+        elif trim == MONTH:
+            clean_datetime = clean_datetime.replace(day=1)
+        elif trim == HOUR:
+            clean_datetime = clean_datetime.replace(hour=_datetime.hour)
+        elif trim == MINUTE:
+            clean_datetime = clean_datetime.replace(hour=_datetime.hour, minute=_datetime.minute)
+        elif trim == SECOND:
+            clean_datetime = clean_datetime.replace(hour=_datetime.hour, minute=_datetime.minute,
+                                                    second=_datetime.second)
+    return clean_datetime
